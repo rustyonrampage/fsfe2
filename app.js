16c959b942fab95a2d53c1022e92a1b422ce006c
Asad <asadabdul.work@gmail.com>
2021-11-07 05:14:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get("/", (req, res)=>res.send("Hello all"));
-app.get("/funny", (req, res)=>res.status( 418 ,"Hello all")).set('x-funny','Hahahaha');
+app.get("/", (req, res) => res.send("Hello all"));
+app.get("/funny", (req, res) => {
+  res.set("x-funny", "Hahahaha").status(418).send("Hahah so funny");
+});
 
-app.listen(port, ()=>console.log(`App runnin on port ${port}`))
+app.listen(port, () => console.log(`App runnin on port ${port}`));
